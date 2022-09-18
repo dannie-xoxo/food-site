9369759cc02f09b2b7c4778c255f9317c91b8ce5
Daniel Bryan <orisaekedanielbryan@gmail.com>
2022-09-18 12:49:51 +0100

    first commit

--- src/components/Nav/navBar.jsx
+++ src/components/Nav/navBar.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../navBar.css';
 import {BsCart2 } from 'react-icons/bs';
 import {BiSearch } from 'react-icons/bi';
 
 function NavBar() {
+    const[active, setActive]= useState('mainNav');
+    const[toggleIcon, setToggleIcon] = useState('toggler');
+
+    const toggle =() => {
+        active=== 'mainNav'
+        ? setActive('mainNav nav-active')
+        : setActive('mainNav')
+
+        toggleIcon==='toggler'
+        ? setToggleIcon ('toggler toggle')
+        : setToggleIcon('toggler');
+    }
     return ( 
         <>
-            <nav className='mainNav'>
+            <nav className='keyNav'>
            <h2 className='brand'>Meals...</h2>
-           <ul className=''>
+           <ul className= {active}>
                 <li className='navItem'><a className='active' href="#Home" >Home</a></li>
                 <li className='navItem'><a href="#menu-container" className='navLink'>Menu</a></li>
                 <li className='navItem'><a href="signIn" className='navLink'>Sign In</a></li>
@@ -17,8 +29,13 @@ function NavBar() {
                 <BsCart2 />
                 <BiSearch />
                 </div>
-
             </ul> 
+            <div onClick={toggle} className={toggleIcon}>
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+            </div>
+            
     </nav>
         </>
      );
